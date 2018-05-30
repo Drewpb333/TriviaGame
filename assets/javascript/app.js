302c873241d24a74dca0468a7da89aafbdb71b0f
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-05-30 13:56:27 -0400

    changes to app.js functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,30 @@
-// var questionSelection;
-// var question;
-// var answer;
-// var userChoice;
+// // var questionSelection;
+// // var question;
+// // var answer;
+// // var userChoice;
 
-//variables for where dynamic content will be placed in index.html
+// //variables for where dynamic content will be placed in index.html
 
-var questionsAndAnswers = $(".questions-and-answers");
-var questionDisplay = $("#question");
-var answerDisplayOne = $("#answer1");
-var answerDisplayTwo = $("#answer2");
-var answerDisplayThree = $("#answer3");
-var answerDisplayFour = $("#answer4");
-var displayQuestionsandTimer = $("#time-remaining, #answer1, #answer2, #answer3, #answer4");
-var timeRemainingJQuery = $("#time-remaining");
-var answerImage = $("#answer-image");
+// var questionsAndAnswers = $(".questions-and-answers");
+// var questionDisplay = $("#question");
+// var answerDisplayOne = $("#answer1");
+// var answerDisplayTwo = $("#answer2");
+// var answerDisplayThree = $("#answer3");
+// var answerDisplayFour = $("#answer4");
+// var displayQuestionsandTimer = $("#time-remaining, #answer1, #answer2, #answer3, #answer4");
+// var timeRemainingJQuery = $("#time-remaining");
+// var answerImage = $("#answer-image");
 
 
-var initTimeRemaining = 30;
-var timer;
+var panel = $("#quiz-area");
+var countStartNumber = 30;
 
 //Array for question, answers, correct answer, and images
 var questions = [{
     question: "What is the most popular pet in the world?",
-    answers: ["Fish", "Cat", "Dog", "Snake"];
+    answers: ["Fish", "Cat", "Dog", "Snake"],
     correctAnswer: "Fish",
-    image = "./assets/images/pet.jpg"
+    image: "./assets/images/pet.jpg"
 }, {
     question: "What do Rin Tin Tin, Lassie, and Snoopy all share in common?",
     answers: ["They're all retrievers.", "They all have their own TV shows.", "They have their own fragrances line.", "They're all labradors."],
@@ -72,31 +72,30 @@ var questions = [{
     image: ".assets/images/frog.jpg"
 }]
 
+var timer;
 
-//game logic
-var gameLogic = {
+var game = {
 
     questions: questions,
     currentQuestion: 0,
-    counter: initTimeRemaining,
+    counter: countStartNumber,
     correct: 0,
     incorrect: 0,
 
-    startTimer: function () {
+    countdown: function () {
         this.counter--;
-        timeRemainingJQuery.text(this.counter);
+        $("counter-number").text(this.counter);
         if (this.counter === 0) {
             console.log("TIME UP");
             this.timeUp();
         }
     },
 
-
-    nextQuestion: function () {
+    loadQuestion: function () {
 
         timer = setInterval(this.countdown.bind(this), 1000);
 
-        questionDisplay("<h2>" + questions[this.currentQuestion].question + "</h2>");
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
         for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
             panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i] +
@@ -104,134 +103,224 @@ var gameLogic = {
         }
     },
 
-    
-}
+    nextQuestion: function () {
+        this.counter = window.countStartNumber;
+        $("#counter-number").text(this.counter);
+        this.currentQuestion++;
+        this.loadQuestion.bind(this)();
+    },
 
+    timeUp: function () {
+        //timer declared globally
+        clearInterval(window.timer);
 
+        $("#counter-number").text(this.counter);
 
+        panel.html("<h2>Out of Time!</h2>");
+        panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+        panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
 
-
-
-
-function newQuestion() {
-    setTimeout(randomQuestionSelection, 5000);
-};
-
-//checks user response
-function correctOrIncorrect() {
-    displayQuestionsandTimer.hide();
-    console.log(correctAnswer);
-    console.log(userChoice);
-    if (userChoice === correctAnswer) {
-        clearInterval(timer);
-        correctResponse++;
-        answerImage.html("<img src='./assets/images/" + questionSelection + ".jpg' alt='" + questionSelection + "'><br><h3>Congratulations!</h3>");
-        answerImage.show();
-        if (totalResponses < 10) {
-            newQuestion();
+        if (this.currentQuestion === questions.length - 1) {
+            setTimeout(this.results, 3 * 1000);
         } else {
-            resetGame()
+            setTimeout(this.nextQuestion, 3 * 1000);
         }
-    } else {
-        clearInterval(timer);
-        incorrectResponse++;
-        answerImage.html("<img src='./assets/images/" + questionSelection + ".jpg' alt='" + questionSelection + "'><br><h3>Wrong!</h3>");
-        answerImage.show();
-        if (totalResponses < 10) {
-            newQuestion();
+    },
+
+    results: function () {
+
+        clearInterval(window.timer);
+
+        panel.html("<h2>All done, heres how you did!</h2>");
+
+        $("#counter-number").text(this.counter);
+
+        panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+        panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+        panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+        panel.append("<br><button id='start-over'>Start Over?</button>");
+    },
+
+    clicked: function (e) {
+        clearInterval(window.timer);
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+            this.answeredCorrectly();
         } else {
-            resetGame()
+            this.answeredIncorrectly();
         }
+    },
+
+    answeredIncorrectly: function () {
+
+        this.incorrect++;
+
+        clearInterval(window.timer);
+
+        panel.html("<h2>Nope!</h2>");
+        panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+        panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+        if (this.currentQuestion === questions.length - 1) {
+            setTimeout(this.results.bind(this), 3 * 1000);
+        } else {
+            setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+        }
+    },
+
+    answeredCorrectly: function () {
+
+        clearInterval(window.timer);
+
+        this.correct++;
+
+        panel.html("<h2>Correct!</h2>");
+        panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+        if (this.currentQuestion === questions.length - 1) {
+            setTimeout(this.results.bind(this), 3 * 1000);
+        } else {
+            setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+        }
+    },
+
+    reset: function () {
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.nextQuestion();
     }
 }
 
-//starts game
-if (startGame == false) {
-    $(document).on("click", "#start-button, #play-again", function (event) {
-        $("#start-button").hide();
-        $("#play-again").hide();
-        event.preventDefault();
-        startGame = true;
-        newQuestion();
-    })
-}
+
+//Event Listeners
+$(document).on("click", "#start-over", game.reset.bind(game));
+
+$(document).on("click", ".answer-button", function (e) {
+    game.clicked.bind(game, e)();
+});
+
+$(document).on("click", "#start", function () {
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+    game.nextQuestion.bind(game)();
+});
 
 
-//comes up with new questions, assigns it to global variable, and removes question from array
-function randomQuestionSelection() {
-    // reset clock
-    answerImage.hide()
-    displayQuestionsandTimer.show();
-    timer = setInterval(function () {
-        initTimeRemaining--;
-        $("#time-remaining").html("Time remaining: " + initTimeRemaining);
-    }, 1000)
-    initTimeRemaining = 30;
-    var questionIndex = Math.floor(Math.random() * (currentQuestionAndAnswerSelection.length - 1));
-    // console.log(questionIndex);
-    questionSelection = currentQuestionAndAnswerSelection[questionIndex];
-    console.log(questionIndex);
-    console.log(questionSelection);
-    // console.log(questionSelection);
-    question = questions[questionSelection];
-    // console.log(question);
-    answerChoices = answers[questionSelection][0];
-    // console.log(answerChoices);
-    correctAnswer = answers[questionSelection][1];
-    // console.log(correctAnswer);
-    currentQuestionAndAnswerSelection.splice(questionIndex, 1);
-    // console.log(currentQuestionAndAnswerSelection);
-    displayQandA();
-}
+// //checks user response
+// function correctOrIncorrect() {
+//     displayQuestionsandTimer.hide();
+//     console.log(correctAnswer);
+//     console.log(userChoice);
+//     if (userChoice === correctAnswer) {
+//         clearInterval(timer);
+//         correctResponse++;
+//         answerImage.html("<img src='./assets/images/" + questionSelection + ".jpg' alt='" + questionSelection + "'><br><h3>Congratulations!</h3>");
+//         answerImage.show();
+//         if (totalResponses < 10) {
+//             newQuestion();
+//         } else {
+//             resetGame()
+//         }
+//     } else {
+//         clearInterval(timer);
+//         incorrectResponse++;
+//         answerImage.html("<img src='./assets/images/" + questionSelection + ".jpg' alt='" + questionSelection + "'><br><h3>Wrong!</h3>");
+//         answerImage.show();
+//         if (totalResponses < 10) {
+//             newQuestion();
+//         } else {
+//             resetGame()
+//         }
+//     }
+// }
 
-function displayQandA() {
-    displayQuestionsandTimer.show();
-    // clear q and a section
-    // change html to Q and A
-    questionDisplay.text(question);
-    answerDisplayOne.text(answerChoices[0]);
-    answerDisplayOne.attr("button-content", answerChoices[0]);
-    answerDisplayTwo.text(answerChoices[1]);
-    answerDisplayTwo.attr("button-content", answerChoices[1]);
-    answerDisplayThree.text(answerChoices[2]);
-    answerDisplayThree.attr("button-content", answerChoices[2]);
-    answerDisplayFour.text(answerChoices[3]);
-    answerDisplayFour.attr("button-content", answerChoices[3]);
-
-    //Time Limit        
-    questionsAndAnswers.on("click", "#answer1", function () {
-        console.log("answer#1");
-        userChoice = answerDisplayOne.attr("button-content");
-        correctOrIncorrect();
-    }).on("click", "#answer2", function () {
-        userChoice = answerDisplayTwo.attr("button-content");
-        correctOrIncorrect();
-    }).on("click", "#answer3", function () {
-        userChoice = answerDisplayThree.attr("button-content");
-        correctOrIncorrect();
-    }).on("click", "#answer4", function () {
-        userChoice = answerDisplayFour.attr("button-content");
-        correctOrIncorrect();
-    })
-}
-
-if (initTimeRemaining === 0) {
-    userChoice = "";
-    correctOrIncorrect();
-};
+// //starts game
+// if (startGame == false) {
+//     $(document).on("click", "#start-button, #play-again", function (event) {
+//         $("#start-button").hide();
+//         $("#play-again").hide();
+//         event.preventDefault();
+//         startGame = true;
+//         newQuestion();
+//     })
+// }
 
 
+// //comes up with new questions, assigns it to global variable, and removes question from array
+// function randomQuestionSelection() {
+//     // reset clock
+//     answerImage.hide()
+//     displayQuestionsandTimer.show();
+//     timer = setInterval(function () {
+//         initTimeRemaining--;
+//         $("#time-remaining").html("Time remaining: " + initTimeRemaining);
+//     }, 1000)
+//     initTimeRemaining = 30;
+//     var questionIndex = Math.floor(Math.random() * (currentQuestionAndAnswerSelection.length - 1));
+//     // console.log(questionIndex);
+//     questionSelection = currentQuestionAndAnswerSelection[questionIndex];
+//     console.log(questionIndex);
+//     console.log(questionSelection);
+//     // console.log(questionSelection);
+//     question = questions[questionSelection];
+//     // console.log(question);
+//     answerChoices = answers[questionSelection][0];
+//     // console.log(answerChoices);
+//     correctAnswer = answers[questionSelection][1];
+//     // console.log(correctAnswer);
+//     currentQuestionAndAnswerSelection.splice(questionIndex, 1);
+//     // console.log(currentQuestionAndAnswerSelection);
+//     displayQandA();
+// }
+
+// function displayQandA() {
+//     displayQuestionsandTimer.show();
+//     // clear q and a section
+//     // change html to Q and A
+//     questionDisplay.text(question);
+//     answerDisplayOne.text(answerChoices[0]);
+//     answerDisplayOne.attr("button-content", answerChoices[0]);
+//     answerDisplayTwo.text(answerChoices[1]);
+//     answerDisplayTwo.attr("button-content", answerChoices[1]);
+//     answerDisplayThree.text(answerChoices[2]);
+//     answerDisplayThree.attr("button-content", answerChoices[2]);
+//     answerDisplayFour.text(answerChoices[3]);
+//     answerDisplayFour.attr("button-content", answerChoices[3]);
+
+//     //Time Limit        
+//     questionsAndAnswers.on("click", "#answer1", function () {
+//         console.log("answer#1");
+//         userChoice = answerDisplayOne.attr("button-content");
+//         correctOrIncorrect();
+//     }).on("click", "#answer2", function () {
+//         userChoice = answerDisplayTwo.attr("button-content");
+//         correctOrIncorrect();
+//     }).on("click", "#answer3", function () {
+//         userChoice = answerDisplayThree.attr("button-content");
+//         correctOrIncorrect();
+//     }).on("click", "#answer4", function () {
+//         userChoice = answerDisplayFour.attr("button-content");
+//         correctOrIncorrect();
+//     })
+// }
+
+// if (initTimeRemaining === 0) {
+//     userChoice = "";
+//     correctOrIncorrect();
+// };
 
 
-// Reset Game
-function resetGame() {
-    clearInterval(timer);
-    questionsAndAnswers.empty();
-    questionsAndAnswers.append("<h2>GAME OVER!</h2>");
-    questionsAndAnswers.append("<h5>Correct Responses: " + correctResponse + "</h5><br><h5>" + incorrectResponse + "</h5>")
-    startGame = false;
-    correctResponse = 0;
-    incorrectResponse = 0;
-    currentQuestionAndAnswerSelection = questionAndAnswerSelection;
-    $("#play-again").show();
-}
+
+
+// // Reset Game
+// function resetGame() {
+//     clearInterval(timer);
+//     questionsAndAnswers.empty();
+//     questionsAndAnswers.append("<h2>GAME OVER!</h2>");
+//     questionsAndAnswers.append("<h5>Correct Responses: " + correctResponse + "</h5><br><h5>" + incorrectResponse + "</h5>")
+//     startGame = false;
+//     correctResponse = 0;
+//     incorrectResponse = 0;
+//     currentQuestionAndAnswerSelection = questionAndAnswerSelection;
+//     $("#play-again").show();
+// }
